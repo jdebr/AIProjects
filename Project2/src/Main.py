327f28793f95776d3d1cc0d2f7d0aac14b6cd973
Shriyansh Kothari <shriyanshk@yahoo.com>
2016-09-28 17:22:51 -0600

    Update Main.py

--- Project2/src/Main.py
+++ Project2/src/Main.py
@@ -2,7 +2,39 @@
 CSCI 446 
 Fall 2016
 Project 2
+Group 3
+
 @author: Joe DeBruycker
 @author: Shriyansh Kothari
 @author: Sara Ounissi
 '''
+
+import random
+
+'''
+One Global Constant Variable for Wumpus World Size
+'''
+
+wumpusWorld = [5,10,15,20,25]
+
+def worldCreation():
+    worldMaker = []
+    sizeSelected = random.choice(wumpusWorld)
+    '''
+    Print statements for Experimental and further design documents too
+    '''
+    print("Currently selected size of Wumpus World is " + str(sizeSelected) + " x " + str(sizeSelected))
+    for i in range(sizeSelected):
+        worldMaker.append([])
+        for j in range(sizeSelected):
+            worldMaker[i].append(j)
+    print("2D Matrix is " + str(worldMaker))
+    return worldMaker
+    
+    
+    
+def main():
+    worldMatrix = worldCreation()
+    
+if __name__ == '__main__':
+    main()
