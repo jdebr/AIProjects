92114a9ab99acd26090500c5d251a1756f917f6b
jdebr <jd.debr@gmail.com>
2016-10-26 12:41:23 -0600

    Initial source for P3

---
+++ Project3/src/project3.py
@@ -0,0 +1,18 @@
+'''
+CSCI 446 
+Fall 2016
+Project 3
+Group 3
+
+@author: Joe DeBruycker
+@author: Shriyansh Kothari
+@author: Sara Ounissi
+'''
+
+
+def main():
+    print("Hello World")
+    
+    
+if __name__ == '__main__':
+    main()
