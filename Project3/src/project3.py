f88768d244d8398f522a14f259198e296c37b03e
jdebr <jd.debr@gmail.com>
2016-11-08 12:22:51 -0700

    Discretization done

--- Project3/src/project3.py
+++ Project3/src/project3.py
@@ -29,9 +29,11 @@ def readingFiles():
                 print(lines.rstrip())
             fileOpen.close()
             
+            
+            
 def readFile(fileName):
-    ''' Reads in the file specified and parses the content into numerical
-    data as a list of lists
+    ''' Reads in the file specified and parses the content into a list of lists,
+    each individual list containing string items representing the text data
     '''
     dataSet = list()
     
@@ -46,14 +48,58 @@ def readFile(fileName):
             dataSet.append(datum)
             
     return dataSet
+
+def discretize(data, numBins):
+    ''' Takes dataset of continuous features and applies equal width binning 
+    to discretize each feature to an integer value numBins
+    '''
+    # Loop for each feature
+    for i in range(len(data[0])):
+        # Find min and max feature values
+        featMin = data[0][i]
+        featMax = data[0][i]
+        for line in data:
+            if line[i] < featMin:
+                featMin = line[i]
+            if line[i] > featMax:
+                featMax = line[i]
+                
+        # Find bin width and bin features
+        binWidth = (featMax - featMin) / numBins
+        for j in range(len(data)):
+            feat = data[j][i]
+            feat = feat - featMin 
+            # Check each bin to see if feature falls within
+            for k in range(1, numBins+1):
+                if feat - (k * binWidth) < 0:
+                    data[j][i] = k
+                    break
+                
+    print(data)
+            
         
+
+def getIris():
+    ''' Processes the Iris data, returning a tuple of 2 lists: ([data],[labels]),
+    where the data has been turned into lists of numerical discretized data and 
+    class labels are strings 
+    '''
+    iris = readFile("../data/iris.data")
+    data = list()
+    labels = list()
+    
+    for item in iris:
+        data.append([float(item[0]), float(item[1]), float(item[2]), float(item[3])])
+        labels.append(item[4])
+        
+    discretize(data, 10)
+    
+    return (data, labels)
         
 
 def main():
-    #readingFiles()
-    fileName = "../data/iris.data"
-    irisData = readFile(fileName)
-    print(irisData)
+    irisData = getIris()
+    #print(irisData)
     
 if __name__ == '__main__':
     main()
