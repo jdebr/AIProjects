8b79ad1b655a206435f37ead4364125ccfb33135
Shriyansh Kothari <shriyanshk@yahoo.com>
2016-11-17 17:08:27 -0700

    Update project3.py
    Implementation begins of Cross Validation

--- Project3/src/project3.py
+++ Project3/src/project3.py
@@ -723,7 +723,7 @@ def testingNaiveBayes(testData):
                 predictionDictionary[posteriorKey] = predictionValue
     
     return (max(predictionDictionary, key=lambda i:predictionDictionary[i]))
-                   
+
                     
 def experiment_NaiveBayes():
     glass = getGlass()
@@ -789,13 +789,41 @@ def knn(data, labels, k):
     #Define Accuracy
     accuracy = (trueVal / len(testData)) * 100
     print(accuracy)
+#5x2 Validation
+def crossValidation(k,n):
+    #for dataSetSize in range (0,n):
     
+    data = getIris()
+    tempDataSet = []
+    dataSet = data[0]
+    labels = data[1]
+    for i in range(len(dataSet)):
+        #tempDataSet.append(dataSet[i])
+        tempDataSet.append((labels[i],dataSet[i]))
+    random.shuffle(tempDataSet)
+    for testRuns in range(0,k):
+        testData = []
+        testLabels = []
+        trainData = []
+        trainLabels = []
+        for i in range(len(tempDataSet)):                
+            num = random.randint(0,len(tempDataSet)-1)
+            #print(num)
+            if( i < 49):
+                trainLabels.append(tempDataSet[num][0])
+                trainData.append(tempDataSet[num][1])
+            elif( i > 49 and i < 150):
+                testLabels.append(tempDataSet[num][0])
+                testData.append(tempDataSet[num][1])
+        #Call method to run Algorithm
+        classSeperation(trainData, trainLabels, testData, testLabels)        
 def main():
     #tests for KNN
     #DATA = getIris()
     #newData = divideData(DATA[0],DATA[1])
     #experiment_ID3()
     experiment_TAN()
+    crossValidation(5, 2)	
     '''
     #This is the block which i used to call Naive Bayes
     dataValues = getGlass()
