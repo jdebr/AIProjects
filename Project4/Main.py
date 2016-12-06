8c2049b9e36134e18d8122fa86ea8b518ef47b05
Shriyansh Kothari <shriyanshk@yahoo.com>
2016-12-06 01:02:50 -0700

    A start
    This is just a rough layout. I will be working more on it + value iteration

--- Project4/Main.py
+++ Project4/Main.py
@@ -11,6 +11,50 @@ Group 3
 from Track import Track
 from Racecar import Racecar
 
+possibleActions = [(1,0),(1,1),(0,1),(-1,0),(-1,-1),(0,-1),(0,0),(1,-1),(-1,1)]
+raceTrack = []
+states = []
+startStates = []
+punishment = -1
+
+def value_Iteration():
+    iterations = 100
+    i = iterations
+    while i > 0:
+        pass
+    
+def racerStates():
+    for row in range(len(raceTrack)):
+        for column in range(len(raceTrack)):
+            if(raceTrack[row][column] != '#'):
+                state = (row,column)
+                states.append(state) 
+
+def racerStart():
+    for row in range(len(raceTrack)):
+        for column in range(len(raceTrack)):
+            if(raceTrack[row][column] == 'S'):
+                state = (row,column)
+                '''
+                Appending Start points so that 
+                we can choose random pints for start
+                '''
+                startStates.append(state)
+
+def actions(state):
+    row,column = state
+    if raceTrack[row][column] == 'F':
+        return exit
+    elif  raceTrack[row][column] == '#':
+        pass
+    return possibleActions
+
+def giveRewards(state):
+    row,column = state
+    if raceTrack[row][column] == 'F':
+        return 100
+    return punishment
+
 def main():
     track = Track('R')
 
