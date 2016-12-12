fda9e9f9abf7575d6ec5f0dbb49e480dcd3de320
jdebr <jd.debr@gmail.com>
2016-12-12 11:00:05 -0700

    stuff

--- Project4/valueIteration.py
+++ Project4/valueIteration.py
@@ -32,9 +32,9 @@ class ValueIteration():
         for pos in self.track.track_positions:
             for vel in self.possible_velocities:
                 temp_state = (pos[0], pos[1], vel[0], vel[1])
-                self.Vtable[temp_state] = 0
-                #for action in self.possible_actions:
-                #    self.Vtable[temp_state][action] = 0
+                self.Vtable[temp_state] = {}
+                for action in self.possible_actions:
+                    self.Vtable[temp_state][action] = 0
         
     def select_action(self):
         # Exploitation
@@ -59,41 +59,37 @@ class ValueIteration():
             '''
             making a copy of states with value 0
             '''
-            self.statesCopy[Initialkey] = 0
-            #for action, value in InititalValue.items():
-            #    self.statesCopy[Initialkey][action] = 0
+            self.statesCopy[Initialkey] = {}
+            for action, value in InititalValue.items():
+                self.statesCopy[Initialkey][action] = 0
 
         #discount = 0.1
         print(len(self.statesCopy))
-        while True:      
-            self.sCopy = self.statesCopy.copy()
+        self.sCopy = self.statesCopy.copy()
+        while True:
             delta = 0
             for key, value in self.Vtable.items():
-                #for move, reward in value.items():
-                    self.statesCopy[key] = self.get_reward(key) + discount * max([sum([probability * self.sCopy[newState] for (probability, newState) in self.stateTransitions(key, a)])
+                for move, reward in value.items():
+                    self.statesCopy[key][move] = self.get_reward(key) + discount * max([sum([probability * self.sCopy[newState][a] for (probability, newState) in self.stateTransitions(key, a)])
                                             for a in self.possible_actions])
-                    delta = max(delta, abs(self.statesCopy[key] - self.sCopy[key]))
-                    
-                    if delta < epsilon * (1-discount) / discount:
-                        print(delta)
-                        print(self.sCopy)
-                        return delta
-                    '''
-                    for Initialkey, InititalValue in self.Vtable.items():
-                        best_action = (0,0)
-                        best_value = self.sCopy[Initialkey][best_action]
-                        self.pi[Initialkey] = {}
-                        for action, value in InititalValue.items():
-                            if value > best_value:
-                                best_action = action
-                                best_value = value
-                        a = best_action 
-                        self.pi[Initialkey][a] = sum([probability * self.sCopy[newState][a] for (probability, newState) in self.stateTransitions(Initialkey, a)])
-                    print(self.pi)
-                    print(len(self.pi))
-                    return self.pi
-                    '''
-                
+                    delta = max(delta, abs(self.statesCopy[key][move] - self.sCopy[key][move]))
+                    #print(self.sCopy[key][move])
+                    #print(self.statesCopy[key][move])
+            #print(self.sCopy)
+            for Initialkey, InititalValue in self.Vtable.items():
+                best_action = (0,0)
+                best_value = self.sCopy[Initialkey][best_action]
+                self.pi[Initialkey] = {}
+                for action, value in InititalValue.items():
+                    if value > best_value:
+                        best_action = action
+                        best_value = value
+                a = best_action 
+                self.pi[Initialkey][a] = sum([probability * self.sCopy[newState][a] for (probability, newState) in self.stateTransitions(Initialkey, a)])
+            print(self.pi)
+            print(len(self.pi))
+            return self.pi
+        
     #Not using for time being                
     def policyIdentification(self, sCopy):
         for Initialkey, InititalValue in self.Vtable.items():
@@ -110,17 +106,12 @@ class ValueIteration():
     
         
     def stateTransitions(self,state,action):
-        
-        #if(action == (0, 0)):
-        #    return[(0.8,state),(0.2,state)]
-        #else:
-            
         self.agent.set_state(state)
         self.agent.set_acceleration(action[0], action[1])
         # Update state
         self.agent.move()
         new_state = self.agent.get_state()
-        return[(0.8,new_state),(0.2,new_state)]
+        return[(0.8,new_state),(0.2,state)]
         '''
         #self.agent.set_state(state)
         self.agent.set_acceleration(action[0], action[1])
